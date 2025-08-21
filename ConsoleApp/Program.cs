ef8e8a9eaed01e58fce82eb518e8c3432bce3124
Valeria <valeriarivera1132165@correo.itm.edu.co>
2025-08-21 09:21:08 -0500

    Creación Empresa y Asociados

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+public class Asociados{
+
+    public int Id;
+    public String? Nombre;
+}
+
+
+
+
+
+
+public class Empresas{
+
+    public int Nit;
+    public String? Nombre;
+    public decimal Ingresos;
+    public Boolean Activa;
+    public Asociados Asociado;
+}
