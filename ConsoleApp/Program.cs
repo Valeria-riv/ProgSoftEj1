619045459979d74951b588c960067e3794003e57
Valeria <valeriarivera1132165@correo.itm.edu.co>
2025-08-21 09:40:30 -0500

    Modificacion clases

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,6 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
 Console.WriteLine("Hello, World!");
 
+Empresas empresa = new Empresas();
+empresa.Nit =1;
+empresa.Nombre = "Colanta";
+empresa.Ingresos = 1.65m;
+empresa.Activa = false;
+empresa.Fecha = DateTime.Now;
+empresa.Asociado = new Asociados() {Id = 1, Nombre = "ColMayor"};
 
 public class Asociados{
 
@@ -8,16 +17,20 @@ public class Asociados{
     public String? Nombre;
 }
 
-
-
-
-
-
 public class Empresas{
 
     public int Nit;
     public String? Nombre;
     public decimal Ingresos;
     public Boolean Activa;
-    public Asociados Asociado;
+    public DateTime Fecha;
+    public Asociados? Asociado;
+    
+}
+
+public class Publicas : Empresas{
+
+    public String? Carnet;
+
+
 }
